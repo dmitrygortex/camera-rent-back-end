af87809cfc89a5796719692d8a3fd9606cb816a6
Admin <Psidjik>
2024-09-24 15:36:48 +0300

    Add migrations

---
+++ Doomsday4.Domain.Data/Migrations/20240924122916_DataMigration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doomsday4.Domain.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EndDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Сost = table.Column<double>(type: "double precision", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    UserRole = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Doomsday4.Domain/Models/Order.cs
+++ Doomsday4.Domain/Models/Order.cs
@@ -4,7 +4,7 @@ namespace Doomsday4.Domain;
 
 public class Order : Entity<Guid>
 {
-    public Order()
+    private Order()
     {
     }
 
@@ -28,8 +28,8 @@ public class Order : Entity<Guid>
 
     // public User User { get; private set;} //зачем прописывать ещё это поле TODO спросить про связи
     // public string UserId { get; private set;}
-    public DateTime StartDateTime { get; private set;}
-    public DateTime EndDateTime { get; private set;}
+    public DateTimeOffset StartDateTime { get; private set;}
+    public DateTimeOffset EndDateTime { get; private set;}
     public double Сost { get; private set; }
     public StatusEnum Status { get; private set;}
     // public List<Equipment> Equipments { get; private set; }= new List<Equipment>();
